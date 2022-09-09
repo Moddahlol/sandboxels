abfbf5482e0ce6dd14ec1a98af895f66a552c111
Moddahlol <113215867+Moddahlol@users.noreply.github.com>
2022-09-09 17:40:53 -0500

    Create Thing.js

---
+++ mods/Thing.js
@@ -0,0 +1,1 @@
+https://github.com/slweeb/sandboxels/tree/main/mods elements.stomach_acid = { color: "#90ee90", behavior: behaviors.LIQUID, category: "other", viscosity: 100000, state: "liquid", density: 720 };
